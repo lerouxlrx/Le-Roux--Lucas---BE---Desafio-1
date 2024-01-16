7a5904262f71130bb06b4efb9bd8a849a188bd48
DevLLR <149336208+DevLLR@users.noreply.github.com>
2024-01-15 22:19:34 -0300

    Desafio 2 V1

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-/* Desafio 1 */
+/* Desafio 2 */
+
+const fs = require('fs').promises;
 
 class ProductManager {
     static id = 0;
 
-    constructor() {
+    constructor(path) {
         this.products = [];
+        this.path = path;
     }
 
-    addProduct(product) {
+    async saveProducts(arrayProducts) {
+      try {
+          await fs.writeFile(this.path, JSON.stringify(arrayProducts, null, 2));
+      } catch (error) {
+          console.log("No se pudo guardar los productos. Error:", error);
+      }
+  }
+
+    async addProduct(product) {
         /* Campos Obligatorios */
         if (!product.title || !product.description || !product.price || !product.thumbnail || !product.code || !product.stock) {
           console.error('Todos los campos son obligatorios.');
@@ -22,55 +33,131 @@ class ProductManager {
     
         /* Id automático */
         const id = ++ProductManager.id;
+
+        /* Crear Objeto */
         const newProduct = { id, ...product };
+
+        /* Agregar al Array */
         this.products.push(newProduct);
         console.log(`Producto agregado: ${newProduct.title}`);
+
+        await this.saveProducts(this.products);
     }
 
-    getProducts() {
-        return this.products
+    async getProducts() {
+        try {
+          const data = await fs.readFile(this.path, 'utf-8');
+          this.products = JSON.parse(data);
+          return this.products;
+        } catch (error) {
+          console.log("No se pudo leer el archivo. Error");
+      }
     }
 
-    getProductById(id) {
+    async getProductById(id) {
+        await this.getProducts()
         const product = this.products.find(item => item.id === id)
 
-        if (product) {
-            console.log(`Se encontró producto con ID: ${id}`);
-            return product;
-        } else {
-            console.log(`Not Found`);
-            return null;
-        }
+        product ? console.log(`Se encontró producto con ID: ${id}`) : console.log(`Not Found`);
+
+        return product
+
     }
+
+    async updateProduct(id, updatedFields) {
+      try {
+        await this.getProducts();
+        const productIndex = this.products.findIndex(item => item.id === id);
+
+        if (productIndex !== -1) {
+          /* Replicar existente */
+          const updatedProduct = { ...this.products[productIndex], ...updatedFields }; 
+          /* Refuerzo ID */
+          updatedProduct.id = id;
+          /* Subir nuevo producto al array */
+          this.products[productIndex] = updatedProduct;
+          /* Push */
+          await this.saveProducts(this.products);
+          console.log(`Producto actualizado con ID: ${id}`);
+        } else {
+          console.error(`Producto con ID ${id} no encontrado`);
+        }
+      } catch (error) {
+        console.log("Error al actualizar producto:", error);
+      }
+    }
+
+    async deleteProduct(id) {
+      try {
+        await this.getProducts();
+        const productIndex = this.products.findIndex(item => item.id === id);
+
+        if (productIndex !== -1) {
+          /* Eliminar */
+          this.products.splice(productIndex, 1);
+
+          await this.saveProducts(this.products);
+          console.log(`Producto eliminado con ID: ${id}`);
+          } else {
+            console.error(`Producto con ID ${id} no encontrado`);
+          }
+      } catch (error) {
+        console.log("Error al eliminar producto:", error);
+      }
+  }
 }
 
 /* Prueba */
+/* Crear instancia */
+  const productManager = new ProductManager("./arrayProductos.json");
 
-const productManager = new ProductManager();
+/* Productos*/
+  const producto1 = {
+    title: "Alargue Blanco",
+    description: "Alargue blanco largo",
+    price: 5000,
+    thumbnail: "https://http2.mlstatic.com/D_NQ_NP_635749-MLA44597218606_012021-O.webp",
+    code: "A01",
+    stock: 100,
+  };
 
-/* Agregar productos */
-productManager.addProduct({
-  title: "Alargue Blanco",
-  description: "Alargue blanco largo",
-  price: 5000,
-  thumbnail: "https://http2.mlstatic.com/D_NQ_NP_635749-MLA44597218606_012021-O.webp",
-  code: "A01",
-  stock: 100,
-});
+  const producto2 = {
+    title: "Alargue Negro",
+    description: "Alargue negro extra largo",
+    price: 10000,
+    thumbnail: "https://http2.mlstatic.com/D_NQ_NP_671786-MLA73593481645_122023-O.webp",
+    code: "A02",
+    stock: 50,
+  };
 
-productManager.addProduct({
-  title: "Alargue Negro",
-  description: "Alargue negro extra largo",
-  price: 10000,
-  thumbnail: "https://http2.mlstatic.com/D_NQ_NP_671786-MLA73593481645_122023-O.webp",
-  code: "A02",
-  stock: 50,
-});
+/* Función Async */
+const exampleFunction = async () => {
+  const testLog =  async () => {
+    const allProducts = await productManager.getProducts();
+    console.log(allProducts);
+  }
 
-/* Mostrar todos los productos. */
-const allProducts = productManager.getProducts();
-console.log("Todos los productos:", allProducts);
+/* Mostrar vacio. */
+  const test1 = await productManager.getProducts();
+  await testLog()
+
+/* Crear Productos */
+  productManager.addProduct(producto1)
+  productManager.addProduct(producto2)
+  await testLog()
 
 /* Mostrar por Id */
-const productById = productManager.getProductById(2);
-console.log("Producto por ID:", productById);
+  const productById = await productManager.getProductById(1);
+  console.log("Producto por ID:", productById);
+
+/* Actualizar producto */
+  const updatedFields = { price: 6000, stock: 80 };
+  await productManager.updateProduct(1, updatedFields);
+  await testLog();
+
+/* Eliminar producto */
+  await productManager.deleteProduct(1);
+  await testLog();
+};
+
+exampleFunction ()
